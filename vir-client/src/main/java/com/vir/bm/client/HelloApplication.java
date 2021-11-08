a01c5136636c896b8fcf22bd28850bd1ba0297e3
daxan <partiz4nn@gmail.com>
2021-11-08 18:57:54 +0100

    Fix jenkins plugins

--- vir-client/src/main/java/com/vir/bm/client/HelloApplication.java
+++ vir-client/src/main/java/com/vir/bm/client/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello test!");
+        stage.setTitle("Hello test2!");
         stage.setScene(scene);
         stage.show();
     }
