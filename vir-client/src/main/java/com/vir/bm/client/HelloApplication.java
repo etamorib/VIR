5e6828a1684866924e7ee2a59454febc5020e716
daxan <partiz4nn@gmail.com>
2021-11-08 19:00:03 +0100

    Test jenkins trigger

--- vir-client/src/main/java/com/vir/bm/client/HelloApplication.java
+++ vir-client/src/main/java/com/vir/bm/client/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello test2!");
+        Scene scene = new Scene(fxmlLoader.load(), 520, 840);
+        stage.setTitle("Hello test4!");
         stage.setScene(scene);
         stage.show();
     }
